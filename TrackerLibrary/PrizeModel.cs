4b057d1d2fb1c6f5d4fee2eaf4b472de9542e464
ceck0z <ceck0z@gmail.com>
2019-05-06 22:33:11 +0300

    TextConnection created 06/05/2019

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -8,12 +8,30 @@ namespace TrackerLibrary
 {
     public class PrizeModel
     {
+        /// <summary>
+        /// The unique identifier for the prize.
+        /// </summary>
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// The numeric identifier for place ( 2 for second place, etc.)
+        /// </summary>
         public int PlaceNumber { get; set; }
 
+        /// <summary>
+        /// The friendly name for place ( second place, first runner up, etc. )
+        /// </summary>
         public string PlaceName { get; set; }
 
+        /// <summary>
+        /// The fixed amount this place earns or zero if it is not used.
+        /// </summary>
         public decimal PrizeAmount { get; set; }
 
+        /// <summary>
+        /// The number that represents the percentage of the overall take or 
+        /// zero if it is not used. The percentage is a function of 1 (so 0.5 for 50%)
+        /// </summary>
         public double PrizePercentage { get; set; }
 
     }
